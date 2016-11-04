2680925927bb30c19fc86192b470a4684ab50b1d
Wei Siang <angweisiang@gmail.com>
2016-11-04 22:48:52 +0800

    Added Comment for EmailController and ReportController

--- CrisisCoreWebApi/Controllers/EmailController.cs
+++ CrisisCoreWebApi/Controllers/EmailController.cs
@@ -10,18 +10,29 @@ namespace CrisisCoreWebApi.Controllers
 {
     public class EmailController : ApiController
     {
+        // Declare string variable for username and password
         string username = "cz3003crisiscore@gmail.com";
         string password = "b514vAIsjLal";
 
+        /// <summary>
+        /// Method to set up email and send
+        /// </summary>
+        /// <param name="emailAdd">Add Recipient email address</param>
+        /// <param name="subject">Add Subject of the email</param>
+        /// <param name="content">Add Content of the email</param>
+        /// <returns></returns>
         [HttpPost]
         public bool SendEmail(string emailAdd, string subject, [FromBody] string content)
         {
+
+            // Set up the sender and recipient email address with subject and the content  
             string from = "cz3003crisiscore@gmail.com";
             MailMessage message = new MailMessage(from, emailAdd);
             message.Subject = subject;
             message.Body = content;
             message.IsBodyHtml = true;
 
+            // Set up the email setting
             SmtpClient client = new SmtpClient();
             client.Port = 587;
             client.Host = "smtp.gmail.com";
@@ -32,11 +43,13 @@ namespace CrisisCoreWebApi.Controllers
 
             try
             {
+                // Send successfully return true with message
                 client.Send(message);
                 return true;
             }
             catch (Exception ex)
             {
+                // Send successfully return false with error message
                 Console.WriteLine(ex.Message);
                 return false;
             }
--- CrisisCoreWebApi/Controllers/ReportController.cs
+++ CrisisCoreWebApi/Controllers/ReportController.cs
@@ -10,28 +10,50 @@ namespace CrisisCoreWebApi.Controllers
 {
     public class ReportController : ApiController
     {
+        /// <summary>
+        /// Generate a status report summarised of key indicators and trends for email
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public string GenerateReport()
         {
+            // Header to show date and time for the report
             string content = "<font face=\"Myriad Pro\"><h1>Report for " + DateTime.Now + "</h1>";
+
             using (AreaController ac = new AreaController())
             using (IncidentTypeController itc = new IncidentTypeController())
             {
+                // Method to get a a list of area in Singapore from the AreaController and initialize to the list areas object
+                //("Central", "North East", "North West", "South East" and "South West")
                 List<Area> areas = ac.GetAllAreas();
 
+                // Loop through the list area object
                 foreach (Area area in areas)
                 {
+                    // Method to get a a list of incident types from the incidentTypeController and initialize to the list incidentTypes object
                     List<IncidentType> incidentTypes = itc.GetIncidentTypesInArea(area.AreaId);
+
+                    // Method to get a list of recent incident types from the incidentTypeController and initialize to the list recentIncidentTypes object
                     List<IncidentType> recentIncidentTypes = itc.GetRecentIncidentTypesInArea(area.AreaId);
+
+                    // Display area name and "New Incidents"
                     content += "<h2>" + area.AreaName + "</h2>";
                     content += "<h3>New Incidents</h3><p>";
+
+                    // Loop through the list recentIncidentTypes object
                     foreach (IncidentType type in recentIncidentTypes)
                     {
+                        // Display recent incident and severity content
                         content += "<strong>" + type.IncidentTypeName + ":</strong> " + type.Severity + "<br />";
                     }
+
+                    // Display "Total Incidents"
                     content += "</p><h3>Total Incidents</h3>";
+
+                    // Loop through the list incidentTypes object
                     foreach (IncidentType type in incidentTypes)
                     {
+                        // Display total incident and severity content
                         content += "<strong>" + type.IncidentTypeName + ":</strong> " + type.Severity + "<br />";
                     }
                     content += "</p>";
