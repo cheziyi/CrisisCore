95ce77db3e6b848ab7b586090e78b1c78a425334
mokwenjun <Tokyo-1997@hotmail.com>
2016-11-04 11:02:49 +0800

    Update FacebookController.cs

--- CrisisCoreWebApi/Controllers/FacebookController.cs
+++ CrisisCoreWebApi/Controllers/FacebookController.cs
@@ -9,17 +9,22 @@ using System.Web.Http;
 namespace CrisisCoreWebApi.Controllers
 {
     public class FacebookController : ApiController
-    {
-        string accessToken = "EAAUPbKRZCyPUBAGyMkowlpPkiQiuAUHPHboE5CAhhgDlpiiKrEjI3WgMgzf1mqOgl9pwBsyK8t4og3VRjbJbP9JafhMfUZAaO1JDZB6kOGG0TtZA0nZAUqH0nMTwFubg6BbWwBjsZBRyZCotbvoZBtgg6YfAfEjsxjJzYL6DqJLYc79Rwf93lLW1";
-        string pageId = "1707287282924587";
+    {   
+       //declare access token
+       string accessToken = "EAAUPbKRZCyPUBAGyMkowlpPkiQiuAUHPHboE5CAhhgDlpiiKrEjI3WgMgzf1mqOgl9pwBsyK8t4og3VRjbJbP9JafhMfUZAaO1JDZB6kOGG0TtZA0nZAUqH0nMTwFubg6BbWwBjsZBRyZCotbvoZBtgg6YfAfEjsxjJzYL6DqJLYc79Rwf93lLW1";
+       //declare page id
+       string pageId = "1707287282924587";
 
         [HttpPost]
         public string PostToPage([FromBody] string update)
         {
             try
             {
+            //sets the ability to pass in an access token
                 var fb = new FacebookClient(accessToken);
+            //post user's message to facebook
                 dynamic result = fb.Post(pageId + "/feed", new { message = update });
+            //returns the result's id
                 return result.id;
             }
             catch (Exception) { }
