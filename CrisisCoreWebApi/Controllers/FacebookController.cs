e0a7687b3f99245ba8dc49b3bd2c1fd258c50dde
mokwenjun <Tokyo-1997@hotmail.com>
2016-11-05 10:23:13 +0800

    Update FacebookController.cs

--- CrisisCoreWebApi/Controllers/FacebookController.cs
+++ CrisisCoreWebApi/Controllers/FacebookController.cs
@@ -1,4 +1,4 @@
-﻿using Facebook;    //for Facebook Graph API, that allows app to read/login/write to Facebook on user's behalf 
+using Facebook;//for Facebook Graph API, that allows app to read/login/write to Facebook on user's behalf 
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,31 +7,32 @@ using System.Net.Http;
 using System.Web.Http;
 
 namespace CrisisCoreWebApi.Controllers
-{
+{   /// <summary>
+    /// Facebook Controller web service class for allowing users to post messages on facebook
+    /// </summary>
     public class FacebookController : ApiController
-    {   
-       //declare user access token which provides CrisisCore facebook app permission to post
-       string accessToken = "EAAUPbKRZCyPUBAGyMkowlpPkiQiuAUHPHboE5CAhhgDlpiiKrEjI3WgMgzf1mqOgl9pwBsyK8t4og3VRjbJbP9JafhMfUZAaO1JDZB6kOGG0TtZA0nZAUqH0nMTwFubg6BbWwBjsZBRyZCotbvoZBtgg6YfAfEjsxjJzYL6DqJLYc79Rwf93lLW1";
-       
-       //facebook page id for CrisisCore app
-       string pageId = "1707287282924587";
-
+    {
+        //declare user access token which provides CrisisCore facebook app permission to post
+        string accessToken = "EAAUPbKRZCyPUBAGyMkowlpPkiQiuAUHPHboE5CAhhgDlpiiKrEjI3WgMgzf1mqOgl9pwBsyK8t4og3VRjbJbP9JafhMfUZAaO1JDZB6kOGG0TtZA0nZAUqH0nMTwFubg6BbWwBjsZBRyZCotbvoZBtgg6YfAfEjsxjJzYL6DqJLYc79Rwf93lLW1";
+        //facebook page id for CrisisCore app
+        string pageId = "1707287282924587";
+        /// <summary>
+        /// Method that allows users to post a message to the facebook page using the app
+        /// </summary>
+        /// <returns> result's id</returns>
         [HttpPost]
         public string PostToPage([FromBody] string update)
         {
             try
             {
-                //delare instance of FacebookClient class for making requests to the Graph API through user access token
+            //delare instance of FacebookClient class for making requests to the Graph API through user access token
                 var fb = new FacebookClient(accessToken);
-                
-                //posts message string to facebook
-                dynamic result = fb.Post(pageId + "/feed", new { message = update });             
-                
-                //check for success message posted on facebook
+            //post message string to facebook
+                dynamic result = fb.Post(pageId + "/feed", new { message = update });
+            //check for success message posted on facebook
                 return result.id;
             }
             catch (Exception) { }
-
             return "";
         }
     }
